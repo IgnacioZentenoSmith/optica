1ae425292f9f7ced687a633b6fc8ca4c3b1c70c5
Ignacio Zenteno Smith <izenteno22@gmail.com>
2023-09-29 22:59:38 -0300

    last

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # NEW
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000","http://localhost:8080",
+    "http://localhost:8000","http://localhost:3000",
 ]
 
 # NEW
