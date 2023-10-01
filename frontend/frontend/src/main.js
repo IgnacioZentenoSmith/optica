1b1941cf371e405b6a6c98385818eece9ed1f190
Ignacio Zenteno Smith <izenteno22@gmail.com>
2023-09-30 21:32:40 -0300

    fixes

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8000/'
+axios.defaults.baseURL = 'http://34.66.54.250/'
 
 Vue.config.productionTip = false
 
